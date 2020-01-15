b4eaf0c17a4fc0f945c6b6fae025df1e830984c3
Pedro Pinho <rihor1000@gmail.com>
2020-01-15 13:36:50 -0300

    Frontend Web completo Dia 2 da semana omnistack 10

---
+++ web/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  // carregar os Devs
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  // cadastrar um novo Dev
+  async function handelAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handelAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
